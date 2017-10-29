1e6705b75c143b61deebfe2e70d83ca163167a34
DESKTOP-KBUNFN3\hirot <hiroto7665@gmail.com>
2017-10-29 16:07:46 +0900

    delete comment in models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+# from django.utils import timezone
+#
+#
+# class Memo(models.Model):
+#     title = models.CharField(max_length=100)
+#     text = models.TextField()
+#     created_date = models.DateTimeField(default=timezone.now)
+#
+#     def __str__(self):
+#         return self.title
